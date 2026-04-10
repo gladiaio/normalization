99c4ddd0f676b13ad01c46b2639b96438f2125a4
egenthon-cmd <egenthon@gladia.io>
2026-04-10 10:45:01 +0200

    feat: added italian language

---
+++ normalization/languages/italian/replacements.py
@@ -0,0 +1,11 @@
+ITALIAN_REPLACEMENTS: dict[str, str] = {
+    "avv": "avvocato",
+    "dott": "dottor",
+    "dr": "dottor",
+    "ecc": "eccetera",
+    "etc": "eccetera",
+    "prof": "professore",
+    "tel": "telefono",
+    "versus": "contro",
+    "vs": "contro",
+}
